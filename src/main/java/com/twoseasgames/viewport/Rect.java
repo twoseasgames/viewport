f9a29eab2cda2a986ee9f8cce936b326d914fb12
David Corticchiato <dawi@hydra>
2013-07-10 16:08:51 +0200

    Revert "Maven artifacts for 1.0"
    This reverts commit 41e83276bf0f549f8bf95926027eaff5c9737bf7.

---
+++ src/main/java/com/twoseasgames/viewport/Rect.java
@@ -0,0 +1,40 @@
+package com.twoseasgames.viewport;
+
+public class Rect {
+
+    private int x;
+    private int y;
+    private int width;
+    private int height;
+    
+    public Rect(int x, int y, int width, int height) {
+        this.x = x;
+        this.y = y;
+        this.width = width;
+        this.height = height;
+    }
+    
+    public int x() {
+        return x;
+    }
+
+    public int y() {
+        return y;
+    }
+
+    public int width() {
+        return width;
+    }
+
+    public int height() {
+        return height;
+    }
+    
+    public void setX(int x) {
+        this.x = x;
+    }
+
+    public void setY(int y) {
+        this.y = y;
+    }
+}
